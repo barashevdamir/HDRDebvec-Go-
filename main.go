500c4f659013fbce3ab18557a19dbe3eda88bfde
Дамир Барашев <barashev-01@mail.ru>
2023-11-24 09:12:18 +0300

    feat(): added weightingFunction

--- main.go
+++ main.go
@@ -6,9 +6,7 @@ import (
 	"sync"
 )
 
-const l = 10
-const Zmin = 0
-const Zmax = 255
+const l = 10 // Коэффициент гладкости
 
 // HDR структура для создания HDR изображений
 type HDR struct {
@@ -18,7 +16,7 @@ type HDR struct {
 	Row    int        // Размер изображений по горизонтали
 	Col    int        // Размер изображений по вертикали
 	L      float32    // Коэффициент гладкости
-
+	W      []float32  // Массив для хранения весов
 }
 
 // newHDR создает новый экземпляр HDR
@@ -60,13 +58,55 @@ func (hdr *HDR) loadImages(filenames []string) {
 
 // alignImages выравнивает изображения
 func (hdr *HDR) alignImages() {
-	// Здесь должен быть код для выравнивания изображений
-	// alignMTB := gocv.NewAlignMTB()...
+	// Создание объекта для выравнивания
+	alignMTB := gocv.NewAlignMTB()
+	defer func(alignMTB *gocv.AlignMTB) {
+		err := alignMTB.Close()
+		if err != nil {
+
+		}
+	}(&alignMTB)
+	// Подготовка слайса из gocv.Mat для выравнивания
+	mats := make([]gocv.Mat, len(hdr.Images))
+	for i, img := range hdr.Images {
+		mats[i] = img.Clone()
+		defer func(mat *gocv.Mat) {
+			err := mat.Close()
+			if err != nil {
+
+			}
+		}(&mats[i])
+	}
+	// Замена исходных изображений выровненными
+	for i := range hdr.Images {
+		err := hdr.Images[i].Close()
+		if err != nil {
+			return
+		} // Освобождение ресурсов исходного изображения
+		hdr.Images[i] = mats[i].Clone() // Копирование выровненного изображения обратно в hdr.Images
+	}
+}
+
+// weightingFunction создает весовую функцию, которая придает больший вес хорошо экспонированным и меньший вес плохо экспонированным пикселям
+func (hdr *HDR) weightingFunction() {
+	Zmin := 0
+	Zmax := 255
+	Zmid := (Zmax + Zmin) / 2
+	// Инициализация среза для весов
+	hdr.W = make([]float32, Zmax-Zmin+1)
+
+	for z := Zmin; z <= Zmax; z++ {
+		if z <= Zmid {
+			hdr.W[z] = float32(z - Zmin + 1)
+		} else {
+			hdr.W[z] = float32(z - Zmin + 1)
+		}
+	}
 }
 
 func main() {
 	filenames := []string{"uploads/image0.jpg", "uploads/image1.jpg", "uploads/image2.jpg"}
-	exposureTimes := []float32{1.0, 0.5, 0.25}
+	exposureTimes := []float32{1 / 6.0, 1.3, 5.0}
 	hdr := NewHDR(filenames, exposureTimes)
 	// Вывод информации об изображениях
 	fmt.Println("Количество изображений:", hdr.N)
@@ -78,5 +118,8 @@ func main() {
 			return
 		}
 	}
+	hdr.weightingFunction()
+	// Вывод значений весовой функции (можно выводить часть массива для проверки)
+	fmt.Println("Пример значений весовой функции:", hdr.W[125:135])
 
 }
